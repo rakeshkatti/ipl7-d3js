89e9931d2446e0b3ff9b822d390b4cabe8d77ff0
rakesh.katti <rakesh.katti@flipkart.com>
2016-05-29 15:06:39 +0530

    All the changes made for ipl 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,114 +1,137 @@
 d3.json('data.json', function(err, data) {
-    var width = 10,
-        height = 30;
-    marginTop = 25;
-    marginLeft = 80;
-    teams_array = ["KKR", "MI", "RCB", "DD", "KXIP", "CSK", "RR", "SRH"];
+	var width = 30,
+	height = 8;
+	marginTop = 45;
+	marginLeft = 80;
+	teams_array = ["RPS", "MI", "KKR", "DD", "GL", "KXIP", "RCB", "SRH"];
 
-    var chart = d3.select(".chart")
-        .attr("width", width * 80)
-        .attr("height",55 * data.length);
+	var chart = d3.select(".chart")
+	.attr("width",111 * data.length)
+	.attr("height", height * 60);
 
-    var path = {};
+	var path = {};
 
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g");
+	var bar = chart.selectAll("g")
+	.data(data)
+	.enter().append("g");
 
-    for (var i = 0; i < teams_array.length; i++) {
-        var teamName = teams_array[i];
-        path[teamName] = [];
+	bar.append("text")
+	.text(function(d, i) {
+		return d["MATCH"];
+	})
+	.attr("y", 10)
+	.attr("x", function(d, i) {
+		return ((i * (marginLeft + width)) + width / 2) - 15;
+	})
+	bar.append("text")
+	.text(function(d, i) {
+		return d["MATCH-TEAMS"];
+	})
+	.attr("y", 25)
+	.attr("x", function(d, i) {
+		return ((i * (marginLeft + width)) + width / 2) - 15;
+	})
 
-        var path1 = {}, path2 = {};
-        bar.append("text")
-            .text(function(d, i) {
-                return d["MATCH"];
-            })
-            .attr("x", 0)
-            .attr("y", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                return (i * (marginTop + height)) + height / 2;
-            })
-        bar.append("text")
-            .text(function(d, i) {
-                return d["MATCH-TEAMS"];
-            })
-            .attr("x", 0)
-            .attr("y", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                return (i * (marginTop + height)) + height;
-            })
+	for (var i = 0; i < teams_array.length; i++) {
+		var teamName = teams_array[i];
+		path[teamName] = [];
 
-        bar.append("rect")
-            .attr("x", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                return d[teamName] * (width + marginLeft);
-            })
-            .attr("y", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                path1.i = {}
-                path2.i = {}
-                path1.i.x = d[teamName] * (width + marginLeft) + (width / 2);
-                path2.i.x = d[teamName] * (width + marginLeft) + (width / 2);
-                path1.i.y = i * (height + marginTop);
-                path2.i.y = (i * (height + marginTop)) + height;
-                path[teamName].push(path1.i);
-                path[teamName].push(path2.i);
-                return i * (marginTop + height);
-            })
-            .attr("width", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                return width;
-            })
-            .attr("height", function(d, i) {
-                if (d[teamName] == null) {
-                    return;
-                }
-                return height;
-            })
-        .attr("class", teamName);
-        // bar.append("svg:image")
-        //         .attr("xlink:href", "http://www.e-pint.com/epint.jpg")
-        //         .attr("width", 150)
-        //         .attr("x", 0)
-        //         .attr("y", function(d, i) {
-        //             if (d[teamName] == null) {
-        //                 return;
-        //             }
-        //             return (i * (marginTop + height)) + height / 2;
-        //         })
-        //         .attr("height", 200);
+		var path1 = {}, path2 = {};
 
-        var lineFunction = d3.svg.line()
-            .x(function(d) {
-                return d.x;
-            })
-            .y(function(d) {
-                return d.y;
-            })
-            .interpolate("monotone");
+		bar.append("rect")
+		.attr("y", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			return d[teamName] * (height + marginTop);
+		})
+		.attr("x", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			path1.i = {}
+			path2.i = {}
+			path1.i.x = d[teamName] * (height + marginTop) + (height / 2);
+			path2.i.x = d[teamName] * (height + marginTop) + (height / 2);
+			path1.i.y = i * (width + marginLeft);
+			path2.i.y = (i * (width + marginLeft)) + width;
+			path[teamName].push(path1.i);
+			path[teamName].push(path2.i);
+			return i * (marginLeft + width);
+		})
+		.attr("height", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			return height;
+		})
+		.attr("width", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			return width;
+		})
+		.attr("class", function(d, i) {
+			var teams = d["MATCH-TEAMS"].split(" ").join("").split("vs");
+			if (teams[0] == teamName) {
+				return teamName + " team current winner";
+			} else if (teams[1] == teamName) {
+				return teamName + " team current looser";
+			} else {
+				return teamName + " team";
+			}
+		})
+		.attr("data-team-name", teamName)
 
-        //The line SVG Path we draw
-        var lineGraph = chart.append("path")
-            .attr("d", lineFunction(path[teamName]))
-            .attr("class", teamName + "path")
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
-    };
+		var lineFunction = d3.svg.line()
+		.y(function(d) {
+			return d.x;
+		})
+		.x(function(d) {
+			return d.y;
+		})
+		.interpolate("monotone");
 
-    // $(".RCB,.RCBpath").on("mouseover",function(e){
-    //     $(".RCB").attr("width",15);
-    //     $(".RCBpath").attr("stroke-width",4);
-    // });
+		//The line SVG Path we draw
+		var lineGraph = chart.append("path")
+		.attr("d", lineFunction(path[teamName]))
+		.attr("class", teamName + "path path")
+		.attr("stroke-width", 2)
+		.attr("data-team-name", teamName)
+		.attr("fill", "none");
+
+		$("." + teamName + ", ." + teamName + "path").on("click",function(e){
+			e.stopPropagation();
+			$(".path, .team").addClass("lighten");
+			var currentTeam = $(this).data('team-name');
+			$("." + currentTeam + ", ." + currentTeam + "path").removeClass("lighten");
+		});
+
+		$(document).on("click", function(e) {
+			$(".path, .team").removeClass("lighten");
+		})
+
+		bar.append("svg:image")
+		.attr("y", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			return (d[teamName] * (height + marginTop)) - 10;
+		})
+		.attr("x", function(d, i) {
+			if (d[teamName] == null) {
+				return;
+			}
+			return (i * (marginLeft + width)) + 10;
+		})
+		.attr("xlink:href", function(d, i) {
+			var teams = d["MATCH-TEAMS"].split(" ").join("").split("vs");
+			if (teams[0] == teamName) {
+				return "../img/trophy.png";
+			}
+		})
+		.attr('width', 10)
+		.attr('height', 10)
+	};
 
 });
